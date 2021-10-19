da2957a897001721ba9519b746ccfd8e389cb316
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-19 15:32:46 +0200

    divido il problema

--- pari_dispari/main.js
+++ pari_dispari/main.js
@@ -1,1 +1,9 @@
-alert("funziona")
+//L'utente sceglie pari o dispari
+
+//inserisce un numero da 1 a 5
+
+//generiamo un numero random da 1 a 5 usando una funzione per il computer
+
+//sommiamo i due numeri
+
+//Stabiliamo se la somma dei 2 numeri è pari o dispari usando una funzione
