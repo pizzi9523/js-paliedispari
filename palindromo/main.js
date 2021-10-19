6111116b563af2ee16306039d5cd7aae7f42108b
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-19 15:30:21 +0200

    Completo palindromo

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,1 +1,29 @@
-alert("funziona")
+// Chiedere all'utente di inserire un parola
+const user_word = prompt("Inserisci una parola");
+
+
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//Creo la funzione
+function palindromVerify(word) {
+    let word_inverse = "";
+    //inverto la parola
+    for (let i = word.length - 1; i >= 0; i--) {
+        word_inverse += word[i]
+    }
+    //console.log(word_inverse);
+    //confronto la parola invertita con quella inserita
+    if (word == word_inverse) {
+        return true;
+    }
+    return false;
+
+}
+
+//richiamo la funzione dentro un if per stampare il risultato della verifica
+if (palindromVerify(user_word)) {
+    console.log(`La parola ${user_word} è palindroma`);
+}
+else {
+    console.log(`La parola ${user_word} non è palindroma`);
+}
